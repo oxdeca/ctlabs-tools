2bcec19f64df7b6c365dea03ce062f372a23d6c1
Wolfgang Schaefer <schaewo@gmail.com>
2026-02-12 00:28:59 -0500

    entry_points

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name="ctlabs-tools",
-    version="0.1.2",
+    version="0.1.3",
     description="ctlabs tools and helpers",
     packages=find_packages(),
     install_requires=[
       "hvac",
       "requests",
+      "streamlit",
+      "pandas",
+      "cryptography",
     ],
     entry_points={
         'console_scripts': [
-            'vault-login=ctlabs_tools.pytest.vault_login:main', # Maps 'vault-login' command to main() function
+            'vault-login=ctlabs_tools.pytest.vault_login:main',
+            'ctlabs-nfs=ctlabs_tools.nfs.app:main',
+            'ctlabs-postfix=ctlabs_tools.postfix.app:main',
+            'ctlabs-kafka=ctlabs_tools.kafka.kafka:main',
+            'ctlabs-smbadc=ctlabs_tools.smbadc.app:main',
+            'ctlabs-slapd=ctlabs_tools.slapd.app:main',
         ],
     },
 )
