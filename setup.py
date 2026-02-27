f7cd2a28bab74c1197955d006a48c21d77b9bb5e
Wolfgang Schaefer <schaewo@gmail.com>
2026-02-27 15:08:14 -0500

    helper

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ctlabs-tools",
-    version="0.1.75",
+    version="0.1.76",
     description="CTLabs tools and helpers",
     packages=find_packages(),
     package_data={
