0a694d1394989d8cc4bb7c73a58cb8b1f55f45b2
Wolfgang Schaefer <schaewo@gmail.com>
2026-03-07 16:51:29 -0500

    vault

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ctlabs-tools",
-    version="0.3.12",
+    version="0.3.13",
     description="CTLabs tools and helpers",
     packages=find_packages(),
     package_data={
